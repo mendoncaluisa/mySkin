8e313965560635b32e789b85dc656d5f8e561a3b
Maria Luísa Mendonça Oliveira <0049545@academico.ifmg.edu.br>
2025-07-11 21:01:49 -0300

    arquivos

---
+++ src/main/java/com/mySkin/MySkinApplication.java
@@ -0,0 +1,13 @@
+package com.mySkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySkinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mySkin/MySkinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mySkin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySkinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
